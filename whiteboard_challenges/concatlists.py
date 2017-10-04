fea079363bab8d5cd459052e40ab53ef8a272348
Catherine Liaw <cat.liaw@gmail.com>
2017-10-03 22:26:02 -0700

    Added different solution and comments after reviewing the solution. Commented out old solution.

--- whiteboard_challenges/concatlists.py
+++ whiteboard_challenges/concatlists.py
@@ -23,9 +23,18 @@ def concat_lists(list1, list2):
 
     # can create new list that is first list, then loop through 2nd list
     # and append on each item in 2nd list,
+    ### actually can just loop through 2nd list and append to first list.
+    ### do not need 3rd list.
     # but can just combine using the + operator, which combines sequences
+    ### this is probably doing the same thing as looping through 2nd list,
+    ### append to 1st list under the hood.
 
-    return list1 + list2
+    for item in list2:
+        list1.append(item)
+
+    return list1
+
+    # return list1 + list2
 
 
 if __name__ == '__main__':
